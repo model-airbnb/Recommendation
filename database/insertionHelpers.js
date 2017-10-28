7cf748f527d81085d6b5afd454660b571ae7ef9a
Tyler Truong <tylertruong@gmail.com>
2017-10-28 11:43:30 -0700

    Spaced out queries over multiple lines.

--- database/insertionHelpers.js
+++ database/insertionHelpers.js
@@ -7,7 +7,10 @@ module.exports.addBookingDetail = (obj) => {
   const {
     listingId, searchId, neighbourhood, roomType, nightlyPrices, market, averageRating,
   } = obj;
-  const bookingText = `INSERT INTO listings (listing_id, market, neighbourhood, room_type, review_scores_rating) VALUES (${listingId}, '${market}', '${neighbourhood}', '${roomType}', ${averageRating}) ON CONFLICT (listing_id) DO NOTHING`;
+  const bookingText = `INSERT INTO listings (listing_id, market, neighbourhood, room_type, review_scores_rating) 
+    VALUES (${listingId}, '${market}', '${neighbourhood}', '${roomType}', ${averageRating}) 
+    ON CONFLICT (listing_id) 
+    DO NOTHING`;
   return client.query(bookingText)
     .then(() => {
       const nightlyEntries = nightlyPrices.map((night) => {
@@ -33,8 +36,12 @@ module.exports.addBookingPrep = (obj) => {
 };
 
 module.exports.addBookingDetailBulk = (bookingArray, nightlyArray) => {
-  const bookingText = `INSERT INTO listings (listing_id, market, neighbourhood, room_type, review_scores_rating) VALUES ${bookingArray.join(', ')} ON CONFLICT (listing_id) DO NOTHING`;
-  const nightlyText = `INSERT INTO booked_nights (listing_id, booked_at, price, search_id) VALUES ${nightlyArray.join(', ')}`;
+  const bookingText = `INSERT INTO listings (listing_id, market, neighbourhood, room_type, review_scores_rating) 
+    VALUES ${bookingArray.join(', ')} 
+    ON CONFLICT (listing_id) 
+    DO NOTHING`;
+  const nightlyText = `INSERT INTO booked_nights (listing_id, booked_at, price, search_id) 
+    VALUES ${nightlyArray.join(', ')}`;
 
   return client.query(bookingText).then(client.query(nightlyText)).catch(err => console.log(err));
 };
@@ -43,7 +50,10 @@ module.exports.addSearchQuery = (obj) => {
   const {
     searchQueryId, timestamp, market, checkIn, checkOut, roomType,
   } = obj;
-  const queryText = `INSERT INTO search_queries (search_id, market, searched_at, check_in, check_out, room_type) VALUES (${searchQueryId}, '${market}', '${timestamp}', '${checkIn}', '${checkOut}',  '${roomType}') ON CONFLICT (search_id) DO NOTHING`;
+  const queryText = `INSERT INTO search_queries (search_id, market, searched_at, check_in, check_out, room_type) 
+    VALUES (${searchQueryId}, '${market}', '${timestamp}', '${checkIn}', '${checkOut}',  '${roomType}') 
+    ON CONFLICT (search_id)
+    DO NOTHING`;
 
   return client.query(queryText);
 };
@@ -53,7 +63,10 @@ module.exports.addSearchResult = (obj) => {
     searchQueryId, availableListings, scoringRules,
   } = obj;
   const results = availableListings.map((result) => {
-    const resultText = `INSERT INTO listings (search_id, listing_id, scoring_rules) VALUES (${searchQueryId}, '${result.listingId}', '${scoringRules}'::json) ON CONFLICT DO NOTHING`;
+    const resultText = `INSERT INTO listings (search_id, listing_id, scoring_rules)
+      VALUES (${searchQueryId}, '${result.listingId}', '${scoringRules}'::json)
+      ON CONFLICT 
+      DO NOTHING`;
 
     return client.query(resultText);
   });
--- database/queryHelpers.js
+++ database/queryHelpers.js
@@ -10,25 +10,34 @@ module.exports.getListingAttributeValues = (string) => {
 };
 
 module.exports.getBookedNightsByDay = (day) => {
-  const queryText = `SELECT * FROM booked_nights WHERE booked_at = '${day}' `;
+  const queryText = `SELECT * FROM booked_nights 
+    WHERE booked_at = '${day}' `;
 
   return client.query(queryText).catch(err => console.log('getBookedNightsByDay', err));
 };
 
 module.exports.getListingByAttribute = (category, string) => {
-  const queryText = `SELECT * FROM listings WHERE ${category} = ${string}`;
+  const queryText = `SELECT * FROM listings 
+    WHERE ${category} = ${string}`;
 
   return client.query(queryText).catch(err => console.log('getListingByAttribute', err));
 };
 
 module.exports.getNumberOfBookedNightsByPrice = (minPrice, maxPrice, startDate, endDate) => {
-  const queryText = `SELECT count(*) FROM booked_nights WHERE price::money::numeric::float8 >= ${minPrice} AND price::money::numeric::float8 <= ${maxPrice} AND booked_at >= '${startDate}' AND booked_at <= '${endDate}'`;
+  const queryText = `SELECT count(*) FROM booked_nights 
+    WHERE price::money::numeric::float8 >= ${minPrice} 
+      AND price::money::numeric::float8 <= ${maxPrice} 
+      AND booked_at >= '${startDate}' 
+      AND booked_at <= '${endDate}'`;
 
   return client.query(queryText).catch(err => console.log('getNumberOfBookedNightsByPrice', err));
 };
 
 module.exports.getBookedNightsByListing = (listingId, startDate, endDate) => {
-  const queryText = `SELECT * FROM booked_nights WHERE listing_id = ${listingId} AND booked_at >= '${startDate}' AND booked_at <= '${endDate}'`;
+  const queryText = `SELECT * FROM booked_nights 
+  WHERE listing_id = ${listingId} 
+    AND booked_at >= '${startDate}' 
+    AND booked_at <= '${endDate}'`;
 
   return client.query(queryText).catch(err => console.log('getBookedNightsByListing', err));
 };
@@ -40,7 +49,11 @@ module.exports.getAveragePriceForSearch = (parameters, startDate, endDate) => {
 
   const queryString = queryArray.join(' AND ');
 
-  const queryText = `SELECT AVG(booked_nights.price::money::numeric::float8) FROM listings INNER JOIN booked_nights ON listings.listing_id = booked_nights.listing_id WHERE ${queryString} AND booked_at >= '${startDate}' AND booked_at <= '${endDate}'`;
+  const queryText = `SELECT AVG(booked_nights.price::money::numeric::float8) FROM listings
+    INNER JOIN booked_nights ON listings.listing_id = booked_nights.listing_id
+    WHERE ${queryString} 
+      AND booked_at >= '${startDate}'
+      AND booked_at <= '${endDate}'`;
   return client.query(queryText).catch(err => console.log('getAllInfoAboutListingCategory', err));
 };
 
